c6bf532a3a79214722d3b43c5246fd93c8e209ee
Willem van Heemstra <wvanheemstra@icloud.com>
2016-02-14 17:19:18 +0000

    Modified

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+ * main.js
+ * 
+ * This module is specific to the application(s)
+ */
+import React from './libraries/react';
+import Router from './libraries/react-router';
+import ReactDOM from './libraries/react-dom';
+import createBrowserHistory from './libraries/history/lib/createBrowserHistory';
+import routes from './routes/creation_ecosystem_route';
+
+let history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>{routes}</Router>, document.getElementById('app')
+);
