c0966c676a397068b1e93ecdb6d5af18ecc58585
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-02-19 16:02:55 +0000

    Modified

--- servers/creations_ecosystem_server.js
+++ servers/creations_ecosystem_server.js
@@ -6,15 +6,28 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser'); // this throws an error about the inherit module not being found
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
+var _ = require('underscore');
+
+// Babel ES6/JSX Compiler
+require('babel-register');
+
+var swig  = require('swig');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var routes = require('../local_routes/creations_ecosystem_routes');
 
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'publications')));
 
 
