bddd820d8ec65d5ae5fd048bb84bf7758ee700cc
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-02-19 14:05:29 +0000

    Modified

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import React from './libraries/react';
 import Router from './libraries/react-router';
 import ReactDOM from './libraries/react-dom';
 import createBrowserHistory from './libraries/history/lib/createBrowserHistory';
-import routes from './routes/creation_ecosystem_route';
+import routes from './local_routes/creation_ecosystem_route';
 
 let history = createBrowserHistory();
 
