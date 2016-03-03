74dae7d60799498246bac0d11a0e80da4fb0aa91
Willem van Heemstra <wvanheemstra@icloud.com>
2016-03-03 22:00:10 +0000

    Modified

--- local_routes/creations-ecosystem-routes.js
+++ local_routes/creations-ecosystem-routes.js
@@ -1,6 +1,6 @@
 import React from '../local_libraries/react';
 import {Route} from '../local_libraries/react-router';
-import App from '../local_applications/CharactersApplication';
+import App from '../local_applications/characters-application';
 import Home from '../local_components/Home';
 console.log('Creations Ecosystem Routes - called');
 
